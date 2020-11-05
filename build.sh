5e0045a0a54b844b76ae7228facff92bc136191d
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-11-06 01:40:49 +0300

    Replace sed with a node script with a single replace

--- build.sh
+++ build.sh
@@ -12,8 +12,7 @@ find package -name "*.ts" -delete
 find package -name "*.map" -delete
 
 rm -rf package/build/src/{events,load-balancer-eds,object-stream,xds-client,xds-bootstrap}.js
-sed -i "/const GoogleAuth = require('google-auth-library')/d" package/build/src/channel-credentials.js
-sed -i "s/    \.GoogleAuth;/throw new Error('unimplemented');/" package/build/src/channel-credentials.js
+node build.replace.js
 
 cd package && find ../patches -type f | sort | xargs -n1 patch -p 1 -i && cd -
 
