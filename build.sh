3e6c0c97c578cbffc6f158bf1b451dbfc597c5c2
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-11-06 01:25:17 +0300

    Add semicolon

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ find package -name "*.map" -delete
 
 rm -rf package/build/src/{events,load-balancer-eds,object-stream,xds-client,xds-bootstrap}.js
 sed -i "/const GoogleAuth = require('google-auth-library')/d" package/build/src/channel-credentials.js
-sed -i "s/    \.GoogleAuth/throw new Error('unimplemented')/" package/build/src/channel-credentials.js
+sed -i "s/    \.GoogleAuth;/throw new Error('unimplemented');/" package/build/src/channel-credentials.js
 
 cd package && find ../patches -type f | sort | xargs -n1 patch -p 1 -i && cd -
 
